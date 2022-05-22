66345b384a50cdc10de4c762c6c1a5a79dd9a5e3
vamshi368 <104982359+vamshi368@users.noreply.github.com>
2022-05-22 13:55:57 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def newmaven()
   {
   sh 'mvn package'
   }
+def newdeploy(ip,contextpath)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/declarativepipeline2/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
+}  
